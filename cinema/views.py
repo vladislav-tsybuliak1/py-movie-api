9a53194194a8f97711faee6ae2bfcd2c242cb823
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-26 17:27:46 +0300

    Fix style by PEP8

--- cinema/views.py
+++ cinema/views.py
@@ -11,7 +11,7 @@ from cinema.serializers import MovieSerializer
 @api_view(["GET", "POST"])
 def movie_list(request: HttpRequest) -> Response:
     if request.method == "GET":
-        movies= Movie.objects.all()
+        movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
