3a88fea8b7298d720ffb3b77a63a49154b564eff
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-26 17:26:05 +0300

    Fix views

--- cinema/views.py
+++ cinema/views.py
@@ -16,10 +16,9 @@ def movie_list(request: HttpRequest) -> Response:
         return Response(serializer.data, status=status.HTTP_200_OK)
     else:
         serializer = MovieSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -30,10 +29,9 @@ def movie_detail(request: HttpRequest, pk: int) -> Response:
         return Response(serializer.data, status=status.HTTP_200_OK)
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     else:
         movie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
