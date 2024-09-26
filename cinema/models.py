1dc3634d842b42951836198f38038f0976b9a6cc
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-09-26 16:54:10 +0300

    Change Movie model, add  magrations file

--- cinema/models.py
+++ cinema/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
-    description = models.TextField
+    description = models.TextField(null=True)
     duration = models.PositiveIntegerField()
